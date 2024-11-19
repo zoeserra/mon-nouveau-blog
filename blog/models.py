21550f1cff3fcb01b954b1d9bfb17dad5e04ba9b
Zoé Serra <serra.zoe@gmail.com>
2024-11-19 13:26:44 +0100

    Mon app La Jungle, premier commit

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ class Equipement(models.Model):
     id_equip = models.CharField(max_length=100, primary_key=True)
     disponibilite = models.CharField(max_length=20)
     photo = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='equipements_photos/', null=True, blank=True)  
+
 
     def __str__(self):
         return self.id_equip
